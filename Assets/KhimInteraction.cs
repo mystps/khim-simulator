3bbab512d3a3b3e41d86e2cd40eec14a1c173deb
Serafima Yurova <serafima.yurova@skyrocket.by>
2023-07-14 01:32:00 +0700

    Initial commit

---
+++ Assets/KhimInteraction.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class KhimInteraction : MonoBehaviour
+{
+    public AudioClip[] sounds; // Array to hold the audio clips for each note
+
+    private AudioSource lastAudioSource; // Track the last played audio source
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(clickPosition, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.CompareTag("Note"))
+            {
+                GameObject noteObject = hit.collider.gameObject;
+                int noteIndex = int.Parse(noteObject.name.Substring(4));
+
+                if (noteIndex >= 1 && noteIndex <= sounds.Length)
+                {
+                    AudioSource audioSource = noteObject.GetComponent<AudioSource>();
+
+                    if (audioSource != null && audioSource.clip != null)
+                    {
+                        int audioIndex = noteIndex - 1;
+
+                        if (lastAudioSource == audioSource && audioSource.isPlaying)
+                        {
+                            audioSource.Stop();
+                            lastAudioSource = null;
+                            Debug.Log("Stopped audio");
+                        }
+                        else
+                        {
+                            audioSource.clip = sounds[audioIndex];
+                            audioSource.Play();
+                            Debug.Log("Played audio");
+
+                            if (lastAudioSource != null && lastAudioSource != audioSource && lastAudioSource.isPlaying)
+                            {
+                                lastAudioSource.Stop();
+                                Debug.Log("Stopped previous audio");
+                            }
+
+                            lastAudioSource = audioSource;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
