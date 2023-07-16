a71dd8bdbdc2e4f8c514e54329b8ee52d53b4903
Serafima Yurova <serafima.yurova@skyrocket.by>
2023-07-16 20:04:41 +0700

    Update KhimInteraction.cs
    Enable touching multiple colliders at the same time and hearing their sounds together

--- Assets/KhimInteraction.cs
+++ Assets/KhimInteraction.cs
@@ -11,43 +11,46 @@ public class KhimInteraction : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(clickPosition, Vector2.zero);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(clickPosition, Vector2.zero);
 
-            if (hit.collider != null && hit.collider.CompareTag("Note"))
+            foreach (RaycastHit2D hit in hits)
             {
-                GameObject noteObject = hit.collider.gameObject;
-                int noteIndex = int.Parse(noteObject.name.Substring(4));
-
-                if (noteIndex >= 1 && noteIndex <= sounds.Length)
+                if (hit.collider != null && hit.collider.CompareTag("Note"))
                 {
-                    AudioSource audioSource = noteObject.GetComponent<AudioSource>();
+                    GameObject noteObject = hit.collider.gameObject;
+                    int noteIndex = int.Parse(noteObject.name.Substring(4));
 
-                    if (audioSource != null && audioSource.clip != null)
+                    if (noteIndex >= 1 && noteIndex <= sounds.Length)
                     {
-                        int audioIndex = noteIndex - 1;
+                        AudioSource audioSource = noteObject.GetComponent<AudioSource>();
 
-                        if (lastAudioSource == audioSource && audioSource.isPlaying)
+                        if (audioSource != null && audioSource.clip != null)
                         {
-                            // If the same note is tapped again and it's already playing, restart the audio
-                            audioSource.Stop();
-                            audioSource.Play();
-                            Debug.Log("Restarted audio");
-                        }
-                        else
-                        {
-                            // If a different note is tapped or the same note is tapped while it's not playing, play the audio
-                            audioSource.clip = sounds[audioIndex];
-                            audioSource.Play();
-                            Debug.Log("Played audio");
+                            int audioIndex = noteIndex - 1;
 
-                            if (lastAudioSource != null && lastAudioSource != audioSource && lastAudioSource.isPlaying)
+                            if (lastAudioSource == audioSource && audioSource.isPlaying)
                             {
-                                // Stop the previous audio source if it's different
-                                lastAudioSource.Stop();
-                                Debug.Log("Stopped previous audio");
+                                // If the same note is tapped again and it's already playing, restart the audio
+                                audioSource.Stop();
+                                audioSource.Play();
+                                Debug.Log("Restarted audio");
                             }
+                            else
+                            {
+                                // If a different note is tapped or the same note is tapped while it's not playing, play the audio
+                                audioSource.clip = sounds[audioIndex];
+                                audioSource.Play();
+                                Debug.Log("Played audio");
 
-                            lastAudioSource = audioSource;
+                                if (lastAudioSource != null && lastAudioSource != audioSource && lastAudioSource.isPlaying)
+                                {
+                                    // Stop the previous audio source if it's different
+                                    lastAudioSource.Stop();
+                                    Debug.Log("Stopped previous audio");
+                                }
+
+                                lastAudioSource = audioSource;
+                            }
                         }
                     }
                 }
